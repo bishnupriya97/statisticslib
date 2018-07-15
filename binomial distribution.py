5bdb5d16e268b8ecbc560e27948a4d173e133913
madgirl-342 <39256029+madgirl-342@users.noreply.github.com>
2018-07-15 10:57:44 +0530

    Create binomial distribution.py

---
+++ binomial distribution.py
@@ -0,0 +1,14 @@
+#to find out the percentage of a given data set which fall under a given probability condition
+
+def fact(n):
+    return 1 if n == 0 else n*fact(n-1)
+
+def comb(n, x):
+    return fact(n) / (fact(x) * fact(n-x))
+
+def b(x, n, p):
+    return comb(n, x) * p**x * (1-p)**(n-x)
+
+l, r = list(map(float, input().split(" ")))
+odds = l / r
+print(round(sum([b(i, 6, odds / (1 + odds)) for i in range(3, 7)]), 3))
